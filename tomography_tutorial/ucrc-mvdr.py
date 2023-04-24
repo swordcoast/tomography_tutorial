af90ab523d2794fd618611a3234b97f758169f23
swordcoast <131719489+swordcoast@users.noreply.github.com>
2023-04-24 19:36:54 -0400

    Create ucrc-mvdr.py

---
+++ tomography_tutorial/ucrc-mvdr.py
@@ -0,0 +1,9 @@
+# This is an attempt to create a function based on the Unit Circle Roots Contrained MVDR algorithm
+
+import os
+import pickle
+import shutil
+import numpy as np
+from osgeo import gdal
+from scipy import ndimage
+import subprocess as sp
